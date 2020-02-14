c8eba656a1644a74a83174be5e4f4926f90d8ac1
peepoo <peepoo@localhost.localdomain>
2020-02-14 19:08:05 +0300

    Обертка для инжекнутых функций из песочницы

---
+++ application.js
@@ -0,0 +1,43 @@
+'use strict';
+//const {lib} = require('./main.js');
+
+//<<<<<<<<<<Dependency Locator list>>>>>>>>>>>>>>
+const fs = require('fs');
+const net = require('net');
+console.dir(require.main, {depth: 0})
+//<<<<<<<<<<----------------------->>>>>>>>>>>>>>
+/*
+console.log("<<<<<<<<<<<<level: 0 -------------------------------\n")
+console.dir(this, {depth:0})
+console.log("<<<<<<<<<<<<level: 0 end-------------------------------\n")
+console.log("<<<<<<<<<<<<level: 1 global-------------------------------\n")
+console.dir({global},{depth:0});
+*/
+
+console.log("<<<<<<<<<<<<level: 1 global end-------------------------------\n")
+console.log("<<<<<<<level:1 fs----------------------------------\n")
+console.dir({fs},{depth: 2});
+console.log("<<<<<<<level:1 fs end-------------------------------\n")
+console.log("<<<<<<<level:1 net----------------------------------\n")
+console.dir({net},{depth: 2});
+console.log("<<<<<<<level:1 net end-------------------------------\n")
+
+//console.dir({api},{depth: 2});
+
+module.exports = () => {
+    api.fs.readFile('./README.md', (err, data) => {
+        if(err){
+            console.log(err.message);
+        }
+        api.console.log(data.toString());
+    });
+    api.timers.setTimeout((arg)=>{
+        if(!arg){
+            api.console.log('Обернутая функция без аргументов');
+        } else {
+            api.console.log('Обернутая функция c аргументами' + arg);
+        }
+    },5000);
+    //setTimeout(()=>{console.log("Стандартная функция")},100)
+    console.log("From lexical scope")
+}
---
+++ framework.js
@@ -0,0 +1,117 @@
+const fs = require('fs');
+const net = require('net');
+const vm = require('vm');
+const timers = require('timers');
+const events = require('events');
+
+const sandboxedFileSystem = require('./lib/main');
+
+const PARSING_TIMEOUT = 1000;
+const EXECUTION_TIMEOUT = 5000;
+global.api = {};
+api.fs = require('fs');
+api.vm = require('vm');
+api.sandboxedFileSystem = sandboxedFileSystem; 
+const {wrapFunction} = require('./lib/wrapFunction.js');
+const {cloneInterface} = require('./lib/cloneInterface.js');
+
+const log = s => {
+    console.log('<<<<<<<<stdout from sanbox start>>>>>>>>>');
+    console.log(s);
+    console.log('<<<<<<<<stdout from sanbox end>>>>>>>>>');
+}
+const safeRequire = name => {
+    if(name === 'fs'){
+        console.log("ACCESS DENIED");
+        const msg = "ACCESS DENIED";
+        return new Error(msg)
+    }
+}
+
+const runSandboxForDIR = path => {
+    const fileName = path + 'application.js';
+    console.log(fileName);
+    const context = {
+        module: {},
+        require: safeRequire,
+        api: {
+            console: {log},
+            timers: {
+                setTimeout: wrapFunction('setTimeout', setTimeout)
+            },
+            fs: cloneInterface(api.sandboxedFileSystem.bind(path))
+        }
+    };
+    context.global =  context;
+    const sandbox = api.vm.createContext(context);
+    api.fs.readFile(fileName, (err, src)=>{
+        const script = new api.vm.Script(src, fileName);
+        const f = script.runInNewContext(sandbox);
+            if(f){
+                f();
+            }
+        });
+    return;
+}
+runSandboxForDIR('./');
+/*
+const context = {
+    module: {},
+    console,
+    setTimeout: (arg) => {
+        return null;
+    },
+    require: name => {
+        if(name === 'fs'){
+            //console.log('Запесоченно'); 
+            return null;
+        }
+        return require(name);
+        //forEach(require.length)
+    },
+    // BLOCK ALL
+    require: name => {
+        return undefined;
+    },
+    // ALLOW ALL
+    require: name => {
+        return require(name)
+    }
+};
+context.global = context;
+const sandbox = vm.createContext(context);
+const api = {timers, events};
+const fileName = './application.js';
+fs.readFile(fileName, 'utf8', (err, src)=>{
+    src = `api => {${src}};`; //<>бу</>
+    let script;
+    try {
+        script = new vm.Script(src, {timeout: PARSING_TIMEOUT});
+    } catch(e){
+        console.log(e);
+        console.log('Parsing error');
+        process.exit(1);
+    }
+    try {
+        const f = script.runInNewContext(sandbox, {timeout: EXECUTION_TIMEOUT}) //custom loading
+        f(api);
+        const exported = sandbox.module.exports;
+        const root = sandbox;
+        console.log("<<<<<<<<<<<<<<< ROOT framework.js START---------------------")
+        //console.dir(root, {depth:0});// obj
+        console.log("<<<<<<<<<<<<<<< ROOT framework.js END---------------------")
+        //console.dir({exported});
+        //console.log('From global');
+        //console.dir({fs,global},{depth: 1});
+        //console.dir({global},{depth: 2});
+        //console.dir({api},{depth: 3});
+    } catch(e){
+        console.log(e);
+        console.log('Parsing error');
+        process.exit(1);
+    }
+})
+*/
+process.on('uncaughtException', err => {
+    console.log("Unhandled exception: " + err);
+})
